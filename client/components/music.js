82a3029ef47f674190b44e96c92cbf37c6811448
Mitchell Drury <Mitchell.Drury@gmail.com>
2019-06-24 20:13:22 -0700

    https

--- client/components/music.js
+++ client/components/music.js
@@ -8,7 +8,7 @@ export default class Home extends Component {
     render () {
         return (
             <div id='music'>
-                <img src={'http://s3.amazonaws.com/readthatmusic/printedMusic/' + this.props.image}/>
+                <img src={'https://s3.amazonaws.com/readthatmusic/printedMusic/' + this.props.image}/>
             </div>
         )
     }
